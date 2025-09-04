f348af2d4a844dd3bfa04f1c3101812b2aae8b65
tanishkagupta-19 <tanishhkkaaa@gmail.com>
2025-09-04 21:51:25 +0530

    tect_cleaner

--- src/processing/text_cleaner.py
+++ src/processing/text_cleaner.py
@@ -1,1 +1,22 @@
- 
+import re
+import string
+import emoji
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+stop_words = set(stopwords.words('english'))
+
+def preprocess_text(text):
+    """Cleans and normalizes raw social media text."""
+    if not isinstance(text, str):
+        return ""
+        
+    text = emoji.demojize(text, delimiters=(" ", " "))
+    text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)
+    text = re.sub(r'\@\w+|\#\w+', '', text)
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    text = text.lower()
+    tokens = word_tokenize(text)
+    filtered_words = [word for word in tokens if word.isalpha() and word not in stop_words]
+    
+    return " ".join(filtered_words)
